9c36053e8d95baa376c3bc63545cf7469b59d675
cunzhong <cunzhong@qq.com>
2016-07-23 19:15:23 +0800

    2016/07/23 19:15

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# fq神器Shadowsocks自动获取密码 
+iss 比較穩定但手動麻煩，就隨便搜到別人的腳本，隨便改成 Python 3.4 ，又順便把編碼改成 utf-8 。
 
-### 自动获取Shadowsocks密码，并打开Shadowsocks.exe （暂时只有windows)
-
-* Shadowsocks 下载地址：http://www.ishadowsocks.net/#download
-> 第一次需要运行Shadowsocks.exe，填写服务器ip，端口密码，选择加密方式，确定后会在当前目录生成gui-config.json文件，
-因为shadowscoks每隔6个小时自动更改密码，所以如果超时之后，需要关闭重新运行shadowsocks.exe，所以写了一个python程序用于自动获取密码并且自动运行exe文件
-
-* 运行Shadowsocks.py 之前须安装python2.7环境
-* 将Shadowsocks.py 跟Shadowsocks.exe 放在同一个文件夹
-* 直接运行 Shadowsocks.py 就可以翻墙了
-* 如果不能正常输出8位数密码则 欢迎在issues里提交反馈，我会在github里第一时间更新此py代码
-
+原說明見 https://github.com/robotbird/Shadowsocks.py
--- iss.py
+++ iss.py
@@ -1,31 +1,25 @@
-import urllib2
-import urllib
+from urllib.request import urlopen
 import re
 import os
 import json
 
-res= urllib2.urlopen("http://www.ishadowsocks.net/")
+res= urlopen("http://www.ishadowsocks.net/")
 con =  res.read().decode("utf-8")
 pattern = re.compile('<section id="free">(.*?)</section>',re.S)
 result  = re.search(pattern,con)
-
-
 
 items = re.findall("<h4>(.*?)</h4>",result.group(0))
 pwdus = items[2][4:12]
 pwdhk = items[8][4:12]
 pwdjp = items[14][4:12]
-print 'init shadowsocks password'
-print pwdus,pwdhk,pwdjp
 
-with open('gui-config.json','r') as ff:
+with open('gui-config.json','r', encoding='utf-8') as ff:
 	d = json.load(ff)
 	d["configs"][0]["password"]=pwdjp
 	d["configs"][1]["password"]=pwdus
 	d["configs"][2]["password"]=pwdhk
 
-with open('gui-config.json','w') as f:
+with open('gui-config.json','w', encoding='utf-8') as f:
 	json.dump(d,f)
-print "open shadowsocks success , please close this window"
 os.popen("Shadowsocks.exe")
 exit()
